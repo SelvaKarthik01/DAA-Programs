79e66c46c13eb4fd981d9caf5c82e26f92cc8ff8
Selvakarthik <selvakarthikfanofvk2004@gmail.com>
2024-04-15 01:52:40 +0530

    Update BellmanFord (1).cpp

--- 2024-04-01/BellmanFord (1).cpp
+++ 2024-04-01/BellmanFord (1).cpp
@@ -4,104 +4,102 @@
 
 using namespace std;
 
-namespace estd {
-    struct node {
-        int from;
-        int to;
-        int weight;
-        node* next;
-    };
+struct node {
+    int from;
+    int to;
+    int weight;
+    node* next;
+};
 
-    struct vnode {
-        string name;
-        int d;
-        int pi;
-    };
+struct vnode {
+    string name;
+    int d;
+    int pi;
+};
 
-    class graph {
-        int n, m;
-        vnode vertex[10];
-        node* elist;
-    public:
-        void createGraph();
-        void INITIALIZE_SINGLE_SOURCE(int s);
-        void RELAX(int u, int v, int w);
-        bool BELLMAN_FORD(int s);
-        void printShortestPaths();
-    };
+class graph {
+    int n, m;
+    vnode vertex[10];
+    node* elist;
+public:
+    void createGraph();
+    void INITIALIZE_SINGLE_SOURCE(int s);
+    void RELAX(int u, int v, int w);
+    bool BELLMAN_FORD(int s);
+    void printShortestPaths();
+};
 
-    void graph::createGraph() {
-        cout << "Enter number of vertices: ";
-        cin >> n;
+void graph::createGraph() {
+    cout << "Enter number of vertices: ";
+    cin >> n;
 
-        for (int i = 1; i <= n; i++) {
-            cout << "Enter name of vertex " << i << ": ";
-            cin >> vertex[i].name;
-        }
-
-        cout << "Enter number of edges: ";
-        cin >> m;
-
-        elist = nullptr;
-        node* last = nullptr;
-
-        for (int j = 1; j <= m; j++) {
-            node* t = new node;
-            cout << "Enter from vertex, to vertex, and weight of edge " << j << ": ";
-            cin >> t->from >> t->to >> t->weight;
-            t->next = nullptr;
-            if (last == nullptr)
-                elist = t;
-            else
-                last->next = t;
-            last = t;
-        }
+    for (int i = 1; i <= n; i++) {
+        cout << "Enter name of vertex " << i << ": ";
+        cin >> vertex[i].name;
     }
 
-    void graph::INITIALIZE_SINGLE_SOURCE(int s) {
-        for (int i = 1; i <= n; i++) {
-            vertex[i].d = INT_MAX;
-            vertex[i].pi = -1;
-        }
-        vertex[s].d = 0;
+    cout << "Enter number of edges: ";
+    cin >> m;
+
+    elist = nullptr;
+    node* last = nullptr;
+
+    for (int j = 1; j <= m; j++) {
+        node* t = new node;
+        cout << "Enter from vertex, to vertex, and weight of edge " << j << ": ";
+        cin >> t->from >> t->to >> t->weight;
+        t->next = nullptr;
+        if (last == nullptr)
+            elist = t;
+        else
+            last->next = t;
+        last = t;
     }
+}
 
-    void graph::RELAX(int u, int v, int w) {
-        if (vertex[v].d > vertex[u].d + w) {
-            vertex[v].d = vertex[u].d + w;
-            vertex[v].pi = u;
-        }
+void graph::INITIALIZE_SINGLE_SOURCE(int s) {
+    for (int i = 1; i <= n; i++) {
+        vertex[i].d = INT_MAX;
+        vertex[i].pi = -1;
     }
+    vertex[s].d = 0;
+}
 
-    bool graph::BELLMAN_FORD(int s) {
-        INITIALIZE_SINGLE_SOURCE(s);
-        for (int i = 1; i <= n - 1; i++) {
-            node* t = elist;
-            while (t != nullptr) {
-                RELAX(t->from, t->to, t->weight);
-                t = t->next;
-            }
-        }
+void graph::RELAX(int u, int v, int w) {
+    if (vertex[v].d > vertex[u].d + w) {
+        vertex[v].d = vertex[u].d + w;
+        vertex[v].pi = u;
+    }
+}
 
+bool graph::BELLMAN_FORD(int s) {
+    INITIALIZE_SINGLE_SOURCE(s);
+    for (int i = 1; i <= n - 1; i++) {
         node* t = elist;
         while (t != nullptr) {
-            if (vertex[t->to].d > vertex[t->from].d + t->weight)
-                return false;
+            RELAX(t->from, t->to, t->weight);
             t = t->next;
         }
-        return true;
     }
 
-    void graph::printShortestPaths() {
-        cout << "Shortest path lengths:\n";
-        for (int i = 1; i <= n; i++) {
-            cout << "Length of vertex " << vertex[i].name << ": " << vertex[i].d << endl;
-        }
+    node* t = elist;
+    while (t != nullptr) {
+        if (vertex[t->to].d > vertex[t->from].d + t->weight)
+            return false;
+        t = t->next;
+    }
+    return true;
+}
+
+void graph::printShortestPaths() {
+    cout << "Shortest path lengths:\n";
+    for (int i = 1; i <= n; i++) {
+        cout << "Length of vertex " << vertex[i].name << ": " << vertex[i].d << endl;
     }
 }
 
 int main() {
-    estd::graph G;
+    graph G;
     G.createGraph();
     int source;
     cout << "Enter source vertex: ";
